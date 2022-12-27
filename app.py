0a67ebff580c5109ff7f5fcc67d61bd583c092d6
ptakpiotr <piter020@outlook.com>
2022-12-27 22:12:17 +0100

    Arrows functionality + adding to scrollbar + algorithm integration

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import tkinter as tk
 import customtkinter as ctk
 import os
 
+import detector
+
 from tkinter import filedialog
 from tkinter import messagebox
 from PIL import Image, ImageTk
@@ -12,6 +14,7 @@ from imagenavigation import ImageNavigation
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 
+
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -21,90 +24,89 @@ class App(ctk.CTk):
         self.all_res = []
         self.current = 0
         self.size = (400, 300)
-  
+
+    def setImage(self, path):
+        im = Image.open(path)
+        im.thumbnail(self.size, Image.Resampling.LANCZOS)
+        self.img = ctk.CTkImage(im, size=im.size)
+        if self.main_img:
+            self.main_img.grid_forget()
+        self.main_img = ctk.CTkLabel(self, text=None, image=self.img)
+        self.main_img.grid(row=1, column=1)
+
     def openfile(self):
         self.main_img.grid_forget()
         self.file = filedialog.askopenfilename(title="Open file", filetypes=[
             ("PNG files", "*.png"),
             ("JPG files", "*.jpg")])
         try:
-            im = Image.open(self.file)
-            im.thumbnail(self.size, Image.Resampling.LANCZOS)
-            self.img = ctk.CTkImage(im, size=im.size)
-            self.main_img = ctk.CTkLabel(self, text=None, image=self.img)
-            self.main_img.grid(row=1, column=1)
+            self.setImage(self.file)
         except Exception as e:
             print(e)
             messagebox.showerror(
                 "Error", "An error has occured while opening the file")
-            
+
     def loadfilesfromdir(self):
         dir = filedialog.askdirectory()
         fi = filter(lambda f: f.endswith(("png", "jpg")), os.listdir(dir))
         for f in fi:
-            self.scrollbar_list.add_image(f)
-            # addToScrollable(f'{dir}/{f}')
+            f_path = f'{dir}/{f}'
+            self.all_images.append(f_path)
+            res = detector.perform_detection(f_path)
+            self.scrollbar_list.add_image(self.setImage, f_path, res)
 
-    # def addToScrollable(self, fi=""):
-    #     try:
-    #         if fi != "":
-    #             self.allImages.append(fi)
-    #         else:
-    #             self.allImages.append(self.file)
-    #         self.allRes.append(1)
-    #         self.msview = msv.MyScrollableView(self.lff)
-    #         self.msview.showimages(self.allImages, self.allRes)
-    #         self.msview.display(0, 0)
-    #     except:
-    #         messagebox.showerror(
-    #             "Error", "An error has occured while performing operation")
+    def addToScrollbar(self):
+        fi = self.file
+        try:
+            res = detector.perform_detection(fi)
+            self.all_images.append(fi)
+            self.scrollbar_list.add_image(self.setImage, fi, res)
+        except:
+            messagebox.showerror(
+                "Error", "An error has occured while performing operation")
 
-    # def arrowClick(self, mode):
-    #     if len(self.allImages) > 0:
-    #         if mode == "L":
-    #             self.current -= 1
-    #             if self.current < 0:
-    #                 self.current = len(self.allImages)-1
-    #         elif mode == "R":
-    #             self.current += 1
-    #             if self.current >= len(self.allImages):
-    #                 self.current = 0
-    #         im = Image.open(self.allImages[self.current])
-    #         im.thumbnail(self.size, Image.Resampling.LANCZOS)
-    #         self.img = ImageTk.PhotoImage(im)
-    #         self.mainImg = Label(self.mff, image=self.img, bg="white")
-    #         self.mainImg.grid(row=0, column=0)
-    #     else:
-    #         messagebox.showerror("Error", "No images loaded")
-        
+    def arrowClick(self, mode):
+        if len(self.all_images) > 0:
+            if mode == "L":
+                self.current -= 1
+                if self.current < 0:
+                    self.current = len(self.all_images)-1
+            elif mode == "R":
+                self.current += 1
+                if self.current >= len(self.all_images):
+                    self.current = 0
+            self.setImage(self.all_images[self.current])
+        else:
+            messagebox.showerror("Error", "No images loaded")
+
     def draw(self):
         # grid layout settings
         # self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=7)
         self.grid_columnconfigure(2, weight=1)
         self.grid_rowconfigure((0, 1, 2), weight=1)
-        
+
         # menu
         self.menu = Menu(self, width=140)
         self.menu.grid(row=0, column=0, rowspan=4, sticky="nwse")
-        
+
         # main image
         self.main_img = ctk.CTkLabel(self, text="Main image")
         self.main_img.grid(row=1, column=1)
-        
-        self.image_navigation = ImageNavigation(self, fg_color="transparent")
+
+        self.image_navigation = ImageNavigation(
+            self, self, fg_color="transparent")
         self.image_navigation.grid(row=2, column=1)
-        
+
         # scrollbar list
         self.scrollbar_list = ScrollbarList(self, width=20)
-        self.scrollbar_list.grid(row=0, column=2, rowspan=3, sticky="nwse")
-        
+        self.scrollbar_list.grid(
+            row=0, column=2, rowspan=3, sticky="nwse")
+
         # start event loop
         self.mainloop()
-        
-        
-        
-        
+
+
 if __name__ == "__main__":
     app = App()
-    app.draw()    
+    app.draw()
--- scrollbarlist.py
+++ scrollbarlist.py
@@ -8,8 +8,10 @@ class ScrollbarList(ctk.CTkFrame):
         self.images_list = []
         self.grid_rowconfigure(1, weight=1)
         self.grid_columnconfigure(0, weight=1)
-        self.scrollbar_label = ctk.CTkLabel(self, text="Your images", font=ctk.CTkFont(size=20, weight="bold"))
-        self.scrollbar_label.grid(row=0, column=0, padx=20, pady=20, sticky="nswe")
+        self.scrollbar_label = ctk.CTkLabel(
+            self, text="Your images", font=ctk.CTkFont(size=20, weight="bold"))
+        self.scrollbar_label.grid(
+            row=0, column=0, padx=20, pady=20, sticky="nswe")
         self.canvas = ctk.CTkCanvas(self, width=140, highlightthickness=0)
         self.canvas.grid(row=1, column=0, sticky="nswe")
         self.canvas.grid_columnconfigure(0, weight=1)
@@ -17,10 +19,12 @@ class ScrollbarList(ctk.CTkFrame):
         self.scrollbar.grid(row=1, column=1, sticky="nswe")
         self.inner_frame = ctk.CTkFrame(self.canvas, corner_radius=0)
         self.inner_frame.grid_columnconfigure(0, weight=1)
-        self.canvas_frame = self.canvas.create_window((0, 0), window=self.inner_frame, anchor="center")
-        
+        self.canvas_frame = self.canvas.create_window(
+            (0, 0), window=self.inner_frame, anchor="center")
+
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
-        self.inner_frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion = self.canvas.bbox("all")))
+        self.inner_frame.bind("<Configure>", lambda e: self.canvas.configure(
+            scrollregion=self.canvas.bbox("all")))
         self.canvas.bind("<Configure>", self.resize_frame)
         self.canvas.bind_all("<MouseWheel>", self.on_mouse_wheel)
 
@@ -28,16 +32,15 @@ class ScrollbarList(ctk.CTkFrame):
         # for i in range(100):
         #     ctk.CTkButton(self.inner_frame, corner_radius=0, border_spacing=5, text=f"image-name{i}.png",
         #                   fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30")).pack(fill="both")
-        
-    def add_image(self, image):
-        self.images_list.append(ctk.CTkButton(self.inner_frame, corner_radius=0, border_spacing=5, text=image,
-                        fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30")))
+
+    def add_image(self, setImage, image, res=""):
+        self.images_list.append(ctk.CTkButton(self.inner_frame, corner_radius=0, border_spacing=5, text=f'{image} {res}',
+                                              fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), command=lambda: setImage(image)))
         self.images_list[-1].pack(fill="both")
-        
-        
+
     def on_mouse_wheel(self, event):
         self.canvas.yview_scroll(-1 * int((event.delta / 120)), "units")
-        
+
     def resize_frame(self, event):
         self.canvas.itemconfig(self.canvas_frame, width=event.width)
         if event.height > self.inner_frame.winfo_height():
