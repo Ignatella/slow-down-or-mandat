9cc93be7fd730076cc724373728d5c0ead1c4e8a
ptakpiotr <piter020@outlook.com>
2023-01-21 22:43:20 +0100

    Comments for detector changed

--- detector.py
+++ detector.py
@@ -7,20 +7,10 @@ from matplotlib import pyplot as plt
 
 
 class PyImageSearchANPR:
-    def __init__(self, min_ar=4, max_ar=5, debug=False):
+    def __init__(self, min_ar=4., max_ar=5.):
         # store the minimum and maximum rectangular aspect ratio
-        # values along with whether we are in debug mode
         self.minAR = min_ar
         self.maxAR = max_ar
-        self.debug = debug
-
-    def debug_imshow(self, title, image):
-        # check to see if we are in debug mode, and if so, show the
-        # image with the supplied title
-        if self.debug:
-            plt.figure()
-            plt.title(title)
-            plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
     def locate_license_plate_candidates(self, gray, keep=5):
         # perform a blackhat morphological operation that will allow
@@ -28,14 +18,12 @@ class PyImageSearchANPR:
         # (i.e., the license plate itself)
         rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13, 5))
         blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, rect_kernel)
-        self.debug_imshow("Blackhat", blackhat)
 
         # next, find regions in the image that are light
         square_kern = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
         light = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, square_kern)
         light = cv2.threshold(
             light, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        self.debug_imshow("Light Regions", light)
 
         # compute the Scharr gradient representation of the blackhat
         # image in the x-direction and then scale the result back to
@@ -45,7 +33,6 @@ class PyImageSearchANPR:
         (minVal, maxVal) = (np.min(grad_x), np.max(grad_x))
         grad_x = 255 * ((grad_x - minVal) / (maxVal - minVal))
         grad_x = grad_x.astype("uint8")
-        self.debug_imshow("Scharr", grad_x)
 
         # blur the gradient representation, applying a closing
         # operation, and threshold the image using Tsutomu's method
@@ -53,20 +40,17 @@ class PyImageSearchANPR:
         grad_x = cv2.morphologyEx(grad_x, cv2.MORPH_CLOSE, rect_kernel)
         thresh = cv2.threshold(
             grad_x, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        self.debug_imshow("Grad Thresh", thresh)
 
         # perform a series of erosion's and dilation's to clean up the
         # threshold image
         thresh = cv2.erode(thresh, None, iterations=2)
         thresh = cv2.dilate(thresh, None, iterations=2)
-        self.debug_imshow("Grad Erode/Dilate", thresh)
 
         # take the bitwise AND between the threshold result and the
         # light regions of the image
         thresh = cv2.bitwise_and(thresh, thresh, mask=light)
         thresh = cv2.dilate(thresh, None, iterations=2)
         thresh = cv2.erode(thresh, None, iterations=1)
-        self.debug_imshow("Final", thresh)
 
         # find contours in the threshold image and sort them by
         # their size in descending order, keeping only the largest
@@ -79,7 +63,7 @@ class PyImageSearchANPR:
         return cnts
 
     def locate_license_plate(self, gray, candidates, clear_border=False):
-        # initialize the license plate contour and ROI
+        # initialize the license plate contour and ROI (Region of interest, potentially many of them)
         lp_cnt = []
         roi = []
 
@@ -94,8 +78,10 @@ class PyImageSearchANPR:
             if self.minAR <= ar <= self.maxAR:
                 # store the license plate contour and extract the
                 # license plate from the grayscale image and then
-                # threshold it
-                lp_cnt.append(c)
+                # threshold it (line 84)
+                # append candidate to the array of candidates
+
+                lp_cnt.append(c) 
                 license_plate = gray[y:y + h, x:x + w]
                 roi.append(cv2.threshold(license_plate, 0, 255,
                                          cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1])
@@ -108,8 +94,6 @@ class PyImageSearchANPR:
                 # display any debugging information and then break
                 # from the loop early since we have found the license
                 # plate region
-                self.debug_imshow("License Plate", license_plate)
-                self.debug_imshow("ROI", roi[-1])
 
         # return a 2-tuple of the license plate ROI and the contour
         # associated with it
@@ -125,18 +109,17 @@ class PyImageSearchANPR:
         return options
 
     def find_and_ocr(self, image, psm=7, clear_border=False):
-        # initialize the license plate text
+        # initialize the license plate text list
         lp_text = []
+
         # convert the input image to grayscale, locate all candidate
         # license plate regions in the image, and then process the
         # candidates, leaving us with the *actual* license plate
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # plt.imshow(cv2.cvtColor(gray, cv2.COLOR_BGR2RGB))
-        # return
+
         candidates = self.locate_license_plate_candidates(gray)
         (lp, lp_cnt) = self.locate_license_plate(
             255-gray, candidates, clear_border=clear_border)
-
 
         # only OCR the license plate if the license plate ROI is not empty
         for l in lp:
@@ -144,21 +127,19 @@ class PyImageSearchANPR:
                 # OCR the license plate
                 options = self.build_tesseract_options(psm=psm)
                 lp_text.append(pytesseract.image_to_string(l, config=options))
-                self.debug_imshow("License Plate", l)
         # return a 2-tuple of the OCR license plate text along with
         # the contour associated with the license plate region
         return lp_text, lp_cnt
 
 
 def cleanup_text(text):
-    # strip out non-ASCII text, so we can draw the text on the image
-    # using OpenCV
+    # strip out non-ASCII text, so we can draw the text on the image (we don't want characters outside the visible range)
     return "".join([c if ord(c) < 128 else "" for c in text]).strip()
 
 
 def perform_detection(imagePath):
     # initialize our ANPR class
-    anpr = PyImageSearchANPR(min_ar=3.5, max_ar=4.7, debug=False)
+    anpr = PyImageSearchANPR(min_ar=3.5, max_ar=4.7)
 
     image = cv2.imread(imagePath)
     image = imutils.resize(image, width=600)
@@ -166,7 +147,8 @@ def perform_detection(imagePath):
 
     # apply automatic license plate recognition
     (lpT, lpC) = anpr.find_and_ocr(image)
-    # only continue if the license plate was successfully OCR'd
+    
+
     if len(lpT) != len(lpC):
         raise Exception(
             "Numbers don\'t match")
@@ -185,10 +167,12 @@ def perform_detection(imagePath):
             (x, y, w, h) = cv2.boundingRect(lpCnt)
             cv2.putText(image, cleanup_text(lpText), (x, y - 15),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
-            # show the output ANPR image
+            
+
             if lpText != None:
                 lpText = sub('[^A-Za-z0-9]+', '', lpText)
             else:
                 lpText = 'ERR'
             res.append(lpText)
-    return res[0] if len(res) > 0 else 'ERR'
+
+    return "; ".join(map(lambda r:str(r),res)) if len(res) > 0 else 'ERR'
