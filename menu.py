e55ce8ba71c557a09786bc62d1a1c43c902768d2
Kacper Syrnik <kacper.syrnik1@gmail.com>
2023-01-21 14:49:38 +0100

    Fix menu icons in dark mode

--- menu.py
+++ menu.py
@@ -6,7 +6,7 @@ class Menu(ctk.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.root = args[0]
-        self.grid_rowconfigure(4, weight=1)
+        self.grid_rowconfigure(6, weight=1)
         self.grid_columnconfigure(0, weight=1)
         self.logo_label = ctk.CTkLabel(
             self, text="Menu", font=ctk.CTkFont(size=20, weight="bold"))
@@ -20,19 +20,19 @@ class Menu(ctk.CTkFrame):
         self.menu_button_3 = MenuItem(self, text="Load from directory", light_image="./assets/icons8-numbers-input-form-48.png",
                                       dark_image="./assets/icons8-numbers-input-form-dark-mode-48.png", command=self.root.loadfilesfromdir, fg_color="transparent")
         self.menu_button_3.grid(row=3, column=0, sticky="ew")
-        self.menu_button_4 = MenuItem(self, text="Add to scrollbar", light_image="./assets/icons8-licence-plate-48.png",
-                                      dark_image="./assets/icons8-licence-plate-48.png", command=self.root.addToScrollbar, fg_color="transparent")
+        self.menu_button_4 = MenuItem(self, text="Add to scrollbar", light_image="./assets/icons8-add-new-48.png",
+                                      dark_image="./assets/icons8-add-new-dark-mode-48.png", command=self.root.addToScrollbar, fg_color="transparent")
         self.menu_button_4.grid(row=4, column=0, sticky="ew")
-        self.menu_button_5 = MenuItem(self, text="Remove from scrollbar", light_image="./assets/icons8-licence-plate-48.png",
-                                      dark_image="./assets/icons8-licence-plate-48.png", command=self.root.removeFromScrollbar, fg_color="transparent")
+        self.menu_button_5 = MenuItem(self, text="Remove from scrollbar", light_image="./assets/icons8-trash-can-48.png",
+                                      dark_image="./assets/icons8-trash-can-dark-mode-48.png", command=self.root.removeFromScrollbar, fg_color="transparent")
         self.menu_button_5.grid(row=5, column=0, sticky="ew")
         self.appearance_mode_label = ctk.CTkLabel(
             self, text="Appearance Mode:", anchor="w")
-        self.appearance_mode_label.grid(row=6, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_label.grid(row=7, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionmenu = ctk.CTkOptionMenu(
             self, values=["Light", "Dark"], command=self.change_appearance_mode_event)
         self.appearance_mode_optionmenu.grid(
-            row=6, column=0, padx=20, pady=(10, 10))
+            row=8, column=0, padx=20, pady=(10, 10))
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         ctk.set_appearance_mode(new_appearance_mode)
